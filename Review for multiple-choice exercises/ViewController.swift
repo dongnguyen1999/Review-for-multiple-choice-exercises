c44287140ca5ffc7bc9a094f0540138ac4bc64ff
TranNhuan <nhuanb1605233@gmail.com>
2021-01-14 19:55:11 -0800

    test branch commit

--- Review for multiple-choice exercises/ViewController.swift
+++ Review for multiple-choice exercises/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    
+    // Dong wrote
     @IBOutlet weak var UsernameLogin: UITextField!
     @IBOutlet weak var BtnLogin: UIButton!
     
