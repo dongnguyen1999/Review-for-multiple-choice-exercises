a5e7468a3c170bb9a46ae7b600889740496442e3
Nhuan <nhuanb1605233@gmail.com>
2021-01-27 01:44:10 -0800

    sua mang hinh search

--- Review for multiple-choice exercises/Controller/FacultySearchController.swift
+++ Review for multiple-choice exercises/Controller/FacultySearchController.swift
@@ -9,16 +9,19 @@ import UIKit
 
 class FacultySearchController: UIViewController, FacultyModelDelegate, UITableViewDelegate, UITableViewDataSource, MajorModelDeledate, SubjectSearchModelDelegate, UISearchBarDelegate {
 
-    //Khai baó
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    //Ánh xạ
     @IBOutlet weak var facultytableView: UITableView!
     @IBOutlet weak var majortableView: UITableView!
     @IBOutlet weak var subjecttableView: UITableView!
-    
     @IBOutlet weak var subjectSearchTableView: UITableView!
+    @IBOutlet weak var btnfaculty: UIButton!
+    @IBOutlet weak var btnmajor: UIButton!
+    @IBOutlet weak var btnsubject: UIButton!
     @IBOutlet weak var searchBar: UISearchBar!
-    
-    
+    @IBOutlet weak var viewbackground: UIView!
+    @IBOutlet weak var stackview: UIStackView!
+    //Khai báo
     var facultysearchmodel : FacultySearchModelView!
     var majorsearchmodel : MajorSearchModelView!
     var subjectsearchmodel :SubjectSearchModelView!
@@ -28,14 +31,15 @@ class FacultySearchController: UIViewController, FacultyModelDelegate, UITableVi
     var subject : [SubjectModel] = []
     var filteredData : [SubjectModel] = []
    
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+      
+      
         facultytableView.delegate = self
         facultytableView.dataSource = self
         facultytableView.isHidden = false
-        
         majortableView.delegate = self
         majortableView.dataSource = self
         majortableView.isHidden = true
@@ -46,90 +50,111 @@ class FacultySearchController: UIViewController, FacultyModelDelegate, UITableVi
         subjectSearchTableView.dataSource = self
         subjectSearchTableView.isHidden = true
         
+        searchBar.delegate = self
         facultysearchmodel=FacultySearchModelView(facultydelegate: self)
         facultysearchmodel.ListFaculty()
         majorsearchmodel = MajorSearchModelView(majormodeldelegate: self)
         subjectsearchmodel = SubjectSearchModelView(subjectmodeldelegate: self)
         
-        //design searchBar
-        searchBar.layer.borderWidth = 1
-        searchBar.layer.borderColor = UIColor.white.cgColor
-        searchBar.delegate = self
-      
-        //custom segment
-        segmentedControl.roundWithBorder(borderWidth: 0, borderColor: UIColor(hex: "#F4F8FF"))
+       
         facultytableView.backgroundColor = UIColor.clear
         majortableView.backgroundColor = UIColor.clear
         subjecttableView.backgroundColor = UIColor.clear
-        //tắt keyboard
-   
+        //Khởi tạo màu button
+        btnfaculty.backgroundColor = UIColor(hex: "9069CD")
+        btnmajor.backgroundColor = UIColor(hex: "F4F8FF")
+        btnsubject.backgroundColor = UIColor(hex: "F4F8FF")
+       //Thay đổi background Image
+        viewbackground?.contentMode = UIView.ContentMode.scaleToFill
+        viewbackground.layer.contents = UIImage(named:"background_history")?.cgImage
+        stackview.layer.contents = UIImage(named:"background_history")?.cgImage
+        searchBar.layer.contents = UIImage(named:"background_history")?.cgImage
+        
+      
+        
     }
+    
    
-    // Tìm kiếm theo tên
+   
+    
+    
+    // Tìm kiếm theo tên môn
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        segmentedControl.selectedSegmentIndex = 2
+       // segmentedControl.selectedSegmentIndex = 2
         subjecttableView.isHidden = true
         facultytableView.isHidden = true
         majortableView.isHidden = true
         subjectSearchTableView.isHidden = false
         subjectsearchmodel.ListSubjectSearch()
+        
         if searchText.isEmpty == false{
+            let filterD = NSPredicate(format: "self contains[d] %@", "hoang")
             filteredData = subject.filter({$0.subjectName.lowercased().contains(searchText.lowercased())})
-            
         }else{
-            segmentedControl.selectedSegmentIndex = 0
+            //segmentedControl.selectedSegmentIndex = 0
             subjecttableView.isHidden = true
             facultytableView.isHidden = false
             majortableView.isHidden = true
             subjectSearchTableView.isHidden = true
-        }
+            }
         
         subjectSearchTableView.reloadData()
         
     }
+    //Cancle tìm kiếm
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.text = ""
         subjectSearchTableView.reloadData()
- 
-    }
-    //menu segment
-    @IBAction func actionSegmented(_ sender: UISegmentedControl) {
-        switch segmentedControl.selectedSegmentIndex {
-        case 0:
-            facultytableView.isHidden = false
-            majortableView.isHidden = true
-            subjecttableView.isHidden = true
-            subjectSearchTableView.isHidden = true
-            major.removeAll()
-            majortableView.reloadData()
-           
-        case 1:
-            facultytableView.isHidden = true
-            majortableView.isHidden = false
-            subjecttableView.isHidden = true
-            major.removeAll()
-            majortableView.reloadData()
-        
-      
-        case 2:
-            facultytableView.isHidden = true
-            majortableView.isHidden = true
-            subjecttableView.isHidden = false
-            subject.removeAll()
-            subjecttableView.reloadData()
-        default:
-            break;
-        }
         
     }
- // table view
+
+    //Sự kiện click button chọn khoa
+    @IBAction func ActionBtnFaculty(_ sender: UIButton) {
+        btnfaculty.backgroundColor = UIColor(hex: "9069CD")
+        btnsubject.backgroundColor = UIColor(hex: "F4F8FF")
+        btnmajor.backgroundColor = UIColor(hex: "F4F8FF")
+        facultytableView.isHidden = false
+        majortableView.isHidden = true
+        subjecttableView.isHidden = true
+        subjectSearchTableView.isHidden = true
+        major.removeAll()
+        majortableView.reloadData()
+    }
+    //Sự kiện click button chọn nghành
+    @IBAction func ActionBtnMajor(_ sender: UIButton) {
+        btnmajor.backgroundColor = UIColor(hex: "9069CD")
+        btnsubject.backgroundColor = UIColor(hex: "F4F8FF")
+        btnfaculty.backgroundColor = UIColor(hex: "9069CD")
+        facultytableView.isHidden = true
+        majortableView.isHidden = false
+        subjecttableView.isHidden = true
+        major.removeAll()
+        majortableView.reloadData()
+        majorsearchmodel.ListMajorSearch()
+    }
+    //Sự kiện click button chọn môn
+    @IBAction func ActionBtnSubject(_ sender: Any) {
+        btnsubject.backgroundColor = UIColor(hex: "9069CD")
+        btnmajor.backgroundColor = UIColor(hex: "9069CD")
+        btnfaculty.backgroundColor = UIColor(hex: "9069CD")
+        facultytableView.isHidden = true
+        majortableView.isHidden = true
+        subjecttableView.isHidden = false
+        subject.removeAll()
+        subjecttableView.reloadData()
+        subjectsearchmodel.ListSubjectSearch()
+    }
+
+    // table view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         var numberOfRow = 1
         switch tableView {
         case facultytableView:
             numberOfRow = faculty.count
         case majortableView:
+          
             numberOfRow = major.count
+            
         case subjecttableView:
             numberOfRow = subject.count
         case subjectSearchTableView:
@@ -145,9 +170,7 @@ class FacultySearchController: UIViewController, FacultyModelDelegate, UITableVi
         var cell = UITableViewCell()
         switch tableView {
         case facultytableView:
-            
             cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
-           // self.facultytableView.deleteRows(at: [indexPath], with: .automatic)
             cell.textLabel?.text = faculty[indexPath.row].facultyName
             cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 18)
             cell.textLabel?.textColor = UIColor(red: 0.565, green: 0.412, blue: 0.804, alpha: 1)
@@ -157,13 +180,13 @@ class FacultySearchController: UIViewController, FacultyModelDelegate, UITableVi
             cell.textLabel?.text = major[indexPath.row].majorName
             cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 18)
             cell.textLabel?.textColor = UIColor(red: 0.565, green: 0.412, blue: 0.804, alpha: 1)
-        
             
         case subjecttableView:
             cell = tableView.dequeueReusableCell(withIdentifier: "subjectcell", for: indexPath)
-          cell.textLabel?.text = subject[indexPath.row].subjectName
+           cell.textLabel?.text = subject[indexPath.row].subjectName
             cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 18)
             cell.textLabel?.textColor = UIColor(red: 0.565, green: 0.412, blue: 0.804, alpha: 1)
+            
         case subjectSearchTableView:
             cell = tableView.dequeueReusableCell(withIdentifier: "subjectsearch", for: indexPath)
             cell.textLabel?.text = filteredData[indexPath.row].subjectName
@@ -171,14 +194,13 @@ class FacultySearchController: UIViewController, FacultyModelDelegate, UITableVi
             cell.textLabel?.textColor = UIColor(red: 0.565, green: 0.412, blue: 0.804, alpha: 1)
         
         default:
-            print("Some things Wrong!!")
+           break
         }
         
             return cell
        
-        
     }
-    //Onclick cell
+    //Sự kiện click cell
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch tableView {
         case facultytableView:
@@ -187,19 +209,22 @@ class FacultySearchController: UIViewController, FacultyModelDelegate, UITableVi
             facultytableView.isHidden = true
             majortableView.isHidden = false
             subjecttableView.isHidden = true
-            segmentedControl.selectedSegmentIndex = 1
-            
-            
-            
+            // Đổi màu button khi chọn
+            btnfaculty.backgroundColor = UIColor(hex: "9069CD")
+            btnmajor.backgroundColor = UIColor(hex: "9069CD")
+            btnsubject.backgroundColor = UIColor(hex: "F4F8FF")
         case majortableView:
+            
             majortableView.deselectRow(at: indexPath, animated: true)
-            //subjectsearchmodel.ListSubject(majorId: 25)
             subjectsearchmodel.ListSubject(majorId: major[indexPath.row].majorId)
             facultytableView.isHidden = true
             majortableView.isHidden = true
             subjecttableView.isHidden = false
-            segmentedControl.selectedSegmentIndex = 2
-            //chuyền id môn sang bài thi
+            // Đổi màu button khi chọn
+            btnfaculty.backgroundColor = UIColor(hex: "9069CD")
+            btnmajor.backgroundColor = UIColor(hex: "9069CD")
+            btnsubject.backgroundColor = UIColor(hex: "9069CD")
+            
         case subjecttableView:
             print("subjectId:\(subject[indexPath.row].subjectId)+Name:\(subject[indexPath.row].subjectName)")
         case subjectSearchTableView:
@@ -207,10 +232,6 @@ class FacultySearchController: UIViewController, FacultyModelDelegate, UITableVi
         default:
            break
         }
-       
-        
-        
-        
        }
    
   
@@ -253,3 +274,4 @@ class FacultySearchController: UIViewController, FacultyModelDelegate, UITableVi
     }
     
 }
+
